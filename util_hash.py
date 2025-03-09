f600ce960aac80af88f0136eef418954e4146846
roniwellington <roniwellington@gmail.com>
2025-03-09 12:01:47 -0300

    Integrando a versão otimizada do assistente de commits

---
+++ util_hash.py
@@ -0,0 +1,11 @@
+import hashlib
+
+def gerar_hash(caminho_arquivo):
+    sha256_hash = hashlib.sha256
+    try:
+        with open(caminho_arquivo, "rb") as arquivo:
+            for byte_block in iter(lambda: arquivo.read(4096), b""):
+                sha256_hash.update(byte_block)
+        return sha256_hash.hexdigest()
+    except FileNotFoundError:
+        return None
